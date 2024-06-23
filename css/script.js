7bc91ef4cb83c03c7c1f72f5c235303ff28cdc2b
Yanadaiah <yanadaiah1993@gmail.com>
2024-06-23 22:52:02 +0530

    Project stracture changes

---
+++ css/script.js
@@ -0,0 +1,12 @@
+
+// JavaScript to handle the small header interactivity
+const smallHeader = document.querySelector('.small-header');
+const headerOptions = document.querySelector('.small-header ul');
+
+function showHeaderOptions() {
+    headerOptions.style.display = 'flex';
+}
+
+function hideHeaderOptions() {
+    headerOptions.style.display = 'none';
+}
